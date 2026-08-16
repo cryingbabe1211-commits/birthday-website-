b177d1cd15e17b58f5fe8a9026dd9855f0aa0765
cryingbabe1211-commits <cryingbabe1211@gmail.com>
2026-08-16 11:21:13 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let message =document.getElementById("message");
 let petals =document.querySelectorAll(".petal");
 button.onclick = function(){
   button.innerHTML = "opened"
- message.innerHTML = "happy birthday! wishing you happiness and peace"
+ message.innerHTML = "happy birthday! whshing you the best year of your life till now🎂🥳👑🎁. And i hust wanna say Sorry for coming into your life well this is my last message to you i know you already have another boy i am here just to tell you sorry and good bye👋✌️"
   button.style.backgroundColor =" #8EC5FF";
   petals.forEach(function(petal){petal.style.display = "block";});
 }
